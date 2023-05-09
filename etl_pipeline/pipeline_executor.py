13339c11c619996ee6d38adfa068942ed1c0ee1b
Florian Blaser <florian.blaser@hotmail.com>
2023-05-09 18:08:34 +0200

    no limits to number of links by default

--- etl_pipeline/pipeline_executor.py
+++ etl_pipeline/pipeline_executor.py
@@ -27,7 +27,7 @@ def get_args():
                         description='Script to execute article extraction pipeline')
 
     parser.add_argument('-c', '--company', default='Credit Suisse')
-    parser.add_argument('-x', '--max-articles', type=int, default=2)
+    parser.add_argument('-x', '--max-articles', type=int)
 
     args = parser.parse_args()
     return args
