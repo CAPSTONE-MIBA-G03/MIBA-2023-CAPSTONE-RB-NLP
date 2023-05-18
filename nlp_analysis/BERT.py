5ceb4b410eff4d527f81e5a45fafa68e4f5d1e64
Florian Blaser <florian.blaser@hotmail.com>
2023-05-18 17:40:18 +0200

    added BERT and BART

--- nlp_analysis/BERT.py
+++ nlp_analysis/BERT.py
@@ -1,25 +1,31 @@
 import torch
 from transformers import BertTokenizer, BertModel
-import nltk
-nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
-
-text1 = "Electric vehicles are becoming increasingly popular. They help reduce greenhouse gas emissions and air pollution. Many governments offer incentives to promote the adoption of electric cars. Charging infrastructure is rapidly expanding in urban areas. The future of transportation seems to be electric."
-text2 = "Renewable energy sources are gaining traction worldwide. Solar and wind power are becoming more cost-effective and efficient. Governments are implementing policies to encourage the use of clean energy. Innovations in energy storage, such as advanced batteries, facilitate the adoption of renewables. The shift towards sustainable energy is gaining momentum."
-
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+from transformers import logging
+logging.set_verbosity_error()
+
 
 def insert_sep_token(text):
     sentences = sent_tokenize(text)
     text_with_sep = ' [SEP] '.join(sentences)
     return text_with_sep
 
+
 def bert_embed_text(text):
+    # if the input is not a string, return an empty list
+    if type(text) != str:
+        return []
     marked_text = "[CLS] " + insert_sep_token(text)
     tokenized_text = tokenizer.tokenize(marked_text)
+
+    if len(tokenized_text) > 512:
+        tokenized_text = tokenized_text[:512]
+
+    # Map the token strings to their vocabulary indeces
     indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
 
-    # Create segment ids
+    # Create segment ids (alternating between 0 and 1)
     segments_ids = []
     current_segment_id = 0
     for value in tokenized_text:
@@ -30,16 +36,16 @@ def bert_embed_text(text):
     tokens_tensor = torch.tensor([indexed_tokens])
     segments_tensors = torch.tensor([segments_ids])
 
-    model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+    model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True) # output all hidden states
     model.eval()
-
-    # https://huggingface.co/transformers/model_doc/bert.html#bertmodel
 
     with torch.no_grad():
         outputs = model(tokens_tensor, segments_tensors)
+        # hidden states from all layers because we set output_hidden_states = True
+        # See the documentation for more details: # https://huggingface.co/transformers/model_doc/bert.html#bertmodel
         hidden_states = outputs[2]
-    
-    # AVeraging second last layer
-    token_vecs = hidden_states[-2][0]
+
+
+    token_vecs = hidden_states[-1][0] # second to last layer
     sentence_embedding = torch.mean(token_vecs, dim=0)
     return sentence_embedding
---
+++ nlp_analysis/NER.py
@@ -0,0 +1,42 @@
+import spacy
+from collections import Counter
+
+def extract_entities(df, w1 : int, w2: int, top_n: int):
+    """
+    Function to extract the top_n most frequently appearing organizations in a text
+    :param text: text to extract organizations from
+    :param w1: weight for title
+    :param w2: weight for description
+    :param top_n: number of organizations to return
+    :return: list of top_n organizations
+    """
+    nlp = spacy.load("en_core_web_lg")
+
+    # adding a pattern that will not classify quantum (capital or lower), AI, Company, quantum computing as organizations
+    patterns = [{'label': 'NORG', 'pattern': 'Quantum'},
+                {'label': 'NORG', 'pattern': 'quantum'},
+                {'label': 'NORG', 'pattern': 'AI'},
+                {'label': 'NORG', 'pattern': 'Company'},
+                {'label': 'NORG', 'pattern': 'quantum computing'},
+                {'label': 'NCORG', 'pattern': 'NYSE'}, # not quantum orgs
+                {'label': 'NCORG', 'pattern': 'NASDAQ'}]
+
+    # adding the patterns to the nlp model, if it is not already there
+    if 'entity_ruler' not in nlp.pipe_names:
+        ruler = nlp.add_pipe("entity_ruler", before="ner")
+        ruler.add_patterns(patterns)
+
+    def get_organizations(text):
+        doc = nlp(text)
+        orgs = [str(ent) for ent in doc.ents if ent.label_ == 'ORG']
+        return orgs
+
+    # Adding a column with organizations for each title, body and description
+    for col in ['title', 'body', 'se_description']:
+        df.loc[:,col+'_orgs'] = df[col].apply(lambda x: get_organizations(x) if type(x) == str else [])
+
+    all_orgs = df['title_orgs'].explode().tolist() * w1 + df['se_description_orgs'].explode().tolist() * w2 + df['body_orgs'].explode().tolist()
+    all_orgs = [org for org in all_orgs if type(org) != float]
+    orgs = Counter(all_orgs)
+    orgs_list = [org[0] for org in orgs.most_common(top_n)]
+    return orgs_list
